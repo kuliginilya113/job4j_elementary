572b626a9e0afb5aa3c164b886e260eda9c2f3f4
Ilya Kuligin <kuligin-ilya@mail.ru>
2023-04-23 11:12:11 +0300

    4.1. Разбор первой программы. [#159033 #192010]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Kuligin Ilya");
+        System.out.println("16.12.1989");
+    }
+}
